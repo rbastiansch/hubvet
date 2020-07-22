cf513a85b3ab765e3398a9b4afd0c095d926e998
Rafael Bastiansch <rafaelbastiansch@gmail.com>
2020-07-22 20:26:27 -0300

    Add lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,16 +2,25 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true,
+    node: true
   },
   extends: [
     '@nuxtjs/eslint-config-typescript',
-    'prettier',
-    'prettier/vue',
-    'plugin:prettier/recommended',
     'plugin:nuxt/recommended',
+    'plugin:vue/recommended'
   ],
   plugins: ['prettier'],
   // add your custom rules here
-  rules: {},
+  rules: {
+    'vue/max-attributes-per-line': [
+      'error',
+      {
+        singleline: 1,
+        multiline: 1
+      }
+    ],
+    'eol-last': 2,
+    'no-multiple-empty-lines': 2,
+    'vue/valid-v-on': 0
+  }
 }
